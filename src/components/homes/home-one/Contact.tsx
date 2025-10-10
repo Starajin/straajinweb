084686dbcb534a7f7944812e9a6242e1ab5c24b2
Solutionsnextwave <gogulan@solutionsnext.in>
2025-10-10 20:03:35 +0530

    Remove phone number and email overlay from Quick Consultation section

--- src/components/homes/home-one/Contact.tsx
+++ src/components/homes/home-one/Contact.tsx
@@ -8,17 +8,6 @@ const Contact = () => {
                <div className="col-lg-6">
                   <div className="contact-thumb1 w-100 h-100 img-custom-anim-right">
                      <img src="assets/img/contact/Contact.jpg" alt="img" className="rounded-4 w-100" />
-                     <div className="cont d-flex justify-content-between align-items-center gap-xxl-5 gap-xl-4 gap-3 wow fadeInUp"
-                        data-wow-delay=".4s">
-                        <div className="">
-                           <span className="fz-14 theme-clr4 d-block">Phone Number</span>
-                           <h6 className="theme-clr4">+880-123-4567</h6>
-                        </div>
-                        <div className="">
-                           <span className="fz-14 theme-clr4 d-block">Email Address</span>
-                           <h6 className="theme-clr4">hello@mail.com</h6>
-                        </div>
-                     </div>
                   </div>
                </div>
                <div className="col-lg-6">
